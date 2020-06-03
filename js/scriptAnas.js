0bda68bfface4feda61bcb08bac46d0bf930ed63
SrRemy99 <annass1999@hotmail.com>
2020-06-03 15:05:07 +0200

    part dels selects

--- js/scriptAnas.js
+++ js/scriptAnas.js
@@ -1,6 +1,27 @@
 var app = {
-    init: function() {
-        
+    init: function () {
+        app.llegirJson();
+    },
+    llegirJson: function () {
+
+        var json = JSON.parse('{ "professors":[ {"nom": "Marc", "classe": "DAM1"}, {"nom": "Marti", "classe": "DAM2"} ],"classes": [ {"nom": "DAM1"}, {"nom": "DAM2"} ],"alumnes": [ {"nom": "Antoni", "classe": "DAM1"}, {"nom": "Antoni", "classe": "DAM1"}, {"nom": "Antoni", "classe": "DAM1"}, {"nom": "Maria", "classe": "DAM2"}, {"nom": "Maria", "classe": "DAM2"}, {"nom": "Maria", "classe": "DAM2"} ] }');
+        app.introduirProfessors(json);
+
+    },
+    introduirProfessors: function (json) {
+        var professors = json.professors;
+        var classes = json.classes;
+        professors.forEach(element => {
+            $("#professor").append("<option value="+element.classe+" > "+element.nom+" </option>");
+        });
+
+        $("#professor").change(function(){
+            classes.forEach(element => {
+                if(element.nom == $(this).val()){
+                    $("#classe").html("<option value="+element.nom+" > "+element.nom+" </option>");
+                }
+            });
+        });
     }
 }
 
